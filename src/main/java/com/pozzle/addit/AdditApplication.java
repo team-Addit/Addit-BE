c6700dc7cb0744fe2e24d93ccd691b4d320092ed
FhRh <yiyop@naver.com>
2025-01-16 21:34:59 +0900

    init : 스프링 부트 및 mariaDB 환경세팅

---
+++ src/main/java/com/pozzle/addit/AdditApplication.java
@@ -0,0 +1,13 @@
+package com.pozzle.addit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pozzle/addit/AdditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pozzle.addit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
