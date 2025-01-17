89925451169cb6debf8050831b88e3a915da13a8
FhRh <yiyop@naver.com>
2025-01-17 14:48:37 +0900

    feat : relay엔티티 생성

---
+++ src/main/java/com/pozzle/addit/relay/entity/Relay.java
@@ -0,0 +1,57 @@
+package com.pozzle.addit.relay.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "relay")
+@EntityListeners(AuditingEntityListener.class)
+public class Relay {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 36, nullable = false, unique = true)
+    private String uuid;
+
+    @Column(length = 36, nullable = false)
+    private String authorId;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Lob
+    private String description;
+
+    private int reactionsCount;
+
+    private int ticklesCount;
+
+    @Enumerated(EnumType.STRING)
+    private RelayStatus status;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/pozzle/addit/relay/entity/RelayStatus.java
@@ -0,0 +1,6 @@
+package com.pozzle.addit.relay.entity;
+
+public enum RelayStatus {
+    ACTIVE,
+    COMPLETED,
+}
