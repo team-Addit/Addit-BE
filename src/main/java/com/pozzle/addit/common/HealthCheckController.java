88670b72f6924e42090a3e7f24ec765ab7e7caf2
FhRh <yiyop@naver.com>
2025-01-17 15:31:25 +0900

    feat : health check용 api 구현

---
+++ src/main/java/com/pozzle/addit/common/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.pozzle.addit.common;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "ok";
+    }
+
+}
