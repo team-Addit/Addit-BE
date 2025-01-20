c2bba9c31c617a254a975b8114f125cf124b52b3
FhRh <yiyop@naver.com>
2025-01-20 16:00:21 +0900

    chore : server를 명시하지 않음

--- src/main/java/com/pozzle/addit/config/SwaggerConfig.java
+++ src/main/java/com/pozzle/addit/config/SwaggerConfig.java
@@ -2,9 +2,6 @@ package com.pozzle.addit.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
-import jakarta.servlet.http.HttpServletRequest;
-import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,25 +9,13 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI(HttpServletRequest request) {
-        String scheme = request.getScheme(); // http or https
-        String host = request.getServerName(); // localhost or domain name
-        int port = request.getServerPort();
-
-        // 포트를 추가해야 하는지 확인
-        String baseUrl = (port == 80 || port == 443)
-            ? String.format("%s://%s", scheme, host)
-            : String.format("%s://%s:%d", scheme, host, port);
-
-        Server server = new Server().url(baseUrl);
-
+    public OpenAPI openAPI() {
         Info info = new Info()
             .version("1.0")
             .title("Addit")
             .description("Addit API");
 
         return new OpenAPI()
-            .info(info)
-            .servers(List.of(server));
+            .info(info);
     }
 }
