dde6ea095d35f28975290834fd3951af34e2851f
FhRh <yiyop@naver.com>
2025-01-20 15:53:24 +0900

    chore : 동적으로 baseurl 감지

--- src/main/java/com/pozzle/addit/config/SwaggerConfig.java
+++ src/main/java/com/pozzle/addit/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.pozzle.addit.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,21 +12,25 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI openAPI(HttpServletRequest request) {
+        String scheme = request.getScheme(); // http or https
+        String host = request.getServerName(); // localhost or domain name
+        int port = request.getServerPort();
+
+        // 포트를 추가해야 하는지 확인
+        String baseUrl = (port == 80 || port == 443)
+            ? String.format("%s://%s", scheme, host)
+            : String.format("%s://%s:%d", scheme, host, port);
+
+        Server server = new Server().url(baseUrl);
+
         Info info = new Info()
             .version("1.0")
             .title("Addit")
             .description("Addit API");
 
-        Server localServer = new Server();
-        localServer.setUrl("http://localhost:8080");
-
-        Server prodServer = new Server();
-        prodServer.setUrl("https://port-0-addit-be-m64o37zm61224fba.sel4.cloudtype.app");
-
-
         return new OpenAPI()
             .info(info)
-            .servers(List.of(localServer, prodServer));
+            .servers(List.of(server));
     }
 }
