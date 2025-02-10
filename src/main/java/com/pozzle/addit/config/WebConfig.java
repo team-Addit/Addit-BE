58178aa20470e01556a8eca1b23ae1d6b2b14cb4
FhRh <yiyop@naver.com>
2025-02-10 15:40:14 +0900

    chore : cors 설정 추가

--- src/main/java/com/pozzle/addit/config/WebConfig.java
+++ src/main/java/com/pozzle/addit/config/WebConfig.java
@@ -3,13 +3,20 @@ package com.pozzle.addit.config;
 import com.pozzle.addit.common.util.OctetStreamReadMsgConverter;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private OctetStreamReadMsgConverter octetStreamReadMsgConverter;
+
+    private final long MAX_AGE_SECS = 3600;
+
+    @Value("${app.cors.allowedOrigins}")
+    private String[] allowedOrigins;
 
     @Autowired
     public WebConfig(OctetStreamReadMsgConverter octetStreamReadMsgConverter) {
@@ -19,5 +26,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(octetStreamReadMsgConverter);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(allowedOrigins)
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(MAX_AGE_SECS);
     }
 }
