beb888b2afe5343546f2e2c39f45637602f4bcce
FhRh <yiyop@naver.com>
2025-02-20 15:22:54 +0900

    fix : 날짜와 ip 기준으로 통계 저장
    - 통계 저장시 로그 추가

--- src/main/java/com/pozzle/addit/mvp/repository/MetricRepository.java
+++ src/main/java/com/pozzle/addit/mvp/repository/MetricRepository.java
@@ -1,6 +1,7 @@
 package com.pozzle.addit.mvp.repository;
 
 import com.pozzle.addit.mvp.entity.Metric;
+import java.time.LocalDate;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MetricRepository extends JpaRepository<Metric, Long> {
 
-    Optional<Metric> findByIp(String ip);
+    Optional<Metric> findByIpAndDate(String ip, LocalDate date);
 
 }
